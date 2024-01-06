f8ded6af56d6da562998466c45956baef556218f
Swapnil bhakare <swapnilbhakare7@gmail.com>
2024-01-06 18:19:20 +0530

    register route created

--- src/app.js
+++ src/app.js
@@ -11,4 +11,13 @@ app.use(express.static("public"));
 
 app.use(cookieParser());
 
+// routes import
+import userRouter from "./routes/user.routes.js";
+
+// routes declaration
+
+app.use("/api/v1/users/", userRouter);
+
+// http://localhost:8080/api/v1/users/register
+
 export default app;
